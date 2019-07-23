b403c98a923f4b20076221e400053945588cd68e
Saif Mode <bhu@hotmail.co.uk>
2019-07-23 19:16:28 +0100

    Add event listeners for keydowns and clicks

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,122 @@
 <template>
   <div id="app">
-    <autocomplete :data="data"></autocomplete>
-    
+    <autocomplete :data="data" @selected="selected = $event"></autocomplete>
+    <p>Selected item: {{ selected }}</p>
   </div>
 </template>
 
 <script>
-import Autocomplete from "./components/Autocomplete.vue"
+import Autocomplete from "./components/Autocomplete.vue";
+
 export default {
-  name: 'app',
+  name: "app",
   components: {
     autocomplete: Autocomplete
   },
-  data () {
+  data() {
     return {
-      msg: 'Welcome to Your Vue.js App',
-      data: [ "Apples", "Apricots", "Avocados", "Bananas", "Boysenberries", "Blueberries", "Bing Cherry", "Cherries", "Cantaloupe", "Crab apples", "Clementine", "Cucumbers", "Damson plum", "Dinosaur Eggs", "Dates", "Dewberries", "Dragon Fruit", "Elderberry", "Eggfruit", "Evergreen Huckleberry", "Entawak", "Fig", "Farkleberry", "Finger Lime", "Grapefruit", "Grapes", "Gooseberries", "Guava", "Honeydew melon", "Hackberry", "Honeycrisp Apples", "Indian Prune", "Indonesian Lime", "Imbe", "Indian Fig", "Jackfruit", "Java Apple", "Jambolan", "Kiwi", "Kaffir Lime", "Kumquat", "Lime", "Lemon", "Longan", "Lychee", "Loquat", "Mango", "Mandarin Orange", "Mulberry", "Melon", "Nectarine", "Navel Orange", "Nashi Pear", "Olive", "Oranges", "Ogeechee Limes", "Oval Kumquat", "Papaya", "Persimmon", "Paw Paw", "Prickly Pear", "Peach", "Pomegranate", "Pineapple", "Quince", "Queen Anne Cherry", "Quararibea cordata", "Rambutan", "Raspberries", "Rose Hips", "Star Fruit", "Strawberries", "Sugar Baby Watermelon", "Tomato", "Tangerine", "Tamarind", "Tart Cherries", "Ugli Fruit", "Uniq Fruit", "Ugni", "Vanilla Bean", "Velvet Pink Banana", "Voavanga", "Watermelon", "Wolfberry", "White Mulberry", "Xigua", "Ximenia caffra fruit", "Xango Mangosteen Fruit Juice", "Yellow Passion Fruit", "Yunnan Hackberry", "Yangmei", "Zig Zag Vine fruit", "Zinfandel Grapes", "Zuchinni" ]
-    }
-  },
-}
+      msg: "Welcome to Your Vue.js App",
+      data: [
+        "Apples",
+        "Apricots",
+        "Avocados",
+        "Bananas",
+        "Boysenberries",
+        "Blueberries",
+        "Bing Cherry",
+        "Cherries",
+        "Cantaloupe",
+        "Crab apples",
+        "Clementine",
+        "Cucumbers",
+        "Damson plum",
+        "Dinosaur Eggs",
+        "Dates",
+        "Dewberries",
+        "Dragon Fruit",
+        "Elderberry",
+        "Eggfruit",
+        "Evergreen Huckleberry",
+        "Entawak",
+        "Fig",
+        "Farkleberry",
+        "Finger Lime",
+        "Grapefruit",
+        "Grapes",
+        "Gooseberries",
+        "Guava",
+        "Honeydew melon",
+        "Hackberry",
+        "Honeycrisp Apples",
+        "Indian Prune",
+        "Indonesian Lime",
+        "Imbe",
+        "Indian Fig",
+        "Jackfruit",
+        "Java Apple",
+        "Jambolan",
+        "Kiwi",
+        "Kaffir Lime",
+        "Kumquat",
+        "Lime",
+        "Lemon",
+        "Longan",
+        "Lychee",
+        "Loquat",
+        "Mango",
+        "Mandarin Orange",
+        "Mulberry",
+        "Melon",
+        "Nectarine",
+        "Navel Orange",
+        "Nashi Pear",
+        "Olive",
+        "Oranges",
+        "Ogeechee Limes",
+        "Oval Kumquat",
+        "Papaya",
+        "Persimmon",
+        "Paw Paw",
+        "Prickly Pear",
+        "Peach",
+        "Pomegranate",
+        "Pineapple",
+        "Quince",
+        "Queen Anne Cherry",
+        "Quararibea cordata",
+        "Rambutan",
+        "Raspberries",
+        "Rose Hips",
+        "Star Fruit",
+        "Strawberries",
+        "Sugar Baby Watermelon",
+        "Tomato",
+        "Tangerine",
+        "Tamarind",
+        "Tart Cherries",
+        "Ugli Fruit",
+        "Uniq Fruit",
+        "Ugni",
+        "Vanilla Bean",
+        "Velvet Pink Banana",
+        "Voavanga",
+        "Watermelon",
+        "Wolfberry",
+        "White Mulberry",
+        "Xigua",
+        "Ximenia caffra fruit",
+        "Xango Mangosteen Fruit Juice",
+        "Yellow Passion Fruit",
+        "Yunnan Hackberry",
+        "Yangmei",
+        "Zig Zag Vine fruit",
+        "Zinfandel Grapes",
+        "Zuchinni"
+      ],
+      selected: ""
+    };
+  }
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
--- src/components/Autocomplete.vue
+++ src/components/Autocomplete.vue
@@ -1,15 +1,21 @@
 <template>
-  <div id="autocomplete">
+  <div>
     <input
       type="text"
       v-model="input"
-      
+      @keydown.down.prevent="down"
+      @keydown.up.prevent="up"
+      @keydown.enter.prevent="selectEnter"
     />
     <ul>
       <li
         v-for="(item, i) in predictions"
         :key="`data-${i}`"
-      >{{ item }}</li>
+        :class="{ selected: i === selectedIndex }"
+        @click="selectClick"
+      >
+        {{ item }}
+      </li>
     </ul>
   </div>
 </template>
@@ -23,7 +29,7 @@ export default {
     return {
       input: "",
       selectedIndex: -1
-    }
+    };
   },
   computed: {
     predictions() {
@@ -32,37 +38,86 @@ export default {
       let searchResults = [];
       if (lettersInSearch > 0) {
         searchResults = this.data.filter(item => {
-          return searchItem == item.toLowerCase().split("").splice(0, lettersInSearch).join("")
-        })
+          return (
+            searchItem ==
+              item
+                .toLowerCase()
+                .split("")
+                .splice(0, lettersInSearch)
+                .join("") && this.input != item
+          );
+        });
       } else {
         return [];
       }
-      return searchResults
+      if (searchResults.length <= this.selectedIndex) {
+        this.selectedIndex = -1;
+      }
+      return searchResults;
     }
   },
+  methods: {
+    down() {
+      if (this.predictions) {
+        this.selectedIndex += 1;
+        this.selectedIndex = this.selectedIndex % this.predictions.length;
+        this.$emit("selection-hover", this.predictions[this.selectedIndex]);
+      }
+    },
+    up() {
+      if (this.predictions) {
+        this.selectedIndex -= 1;
+        this.selectedIndex =
+          (this.selectedIndex + this.predictions.length) %
+          this.predictions.length; // prevents negative index which would cause modulo wrap to fail
+        this.$emit("selection-hover", this.predictions[this.selectedIndex]);
+      }
+    },
+    selectEnter() {
+      this.input = this.predictions[this.selectedIndex];
+      this.select();
+    },
+    selectClick() {
+      this.input = event.target.innerText;
+      this.select();
+    },
+    select() {
+      this.$emit("selected", this.input);
+    }
+  }
 };
 </script>
 
 <style>
-  input[type="text"] {
-    width: 233px;
-    height: 21px;
-    padding: 5px 8px;
-    outline: none;
-  }
+input[type="text"] {
+  width: 233px;
+  height: 21px;
+  padding: 5px 8px;
+  outline: none;
+}
 
-  ul {
-    display: flex;
-    flex-direction: column;
-    margin: 0;
-    padding: 0;
-    width: 233px;
-    list-style: none;
-  }
+ul {
+  display: flex;
+  flex-direction: column;
+  position: absolute;
+  margin: 0;
+  padding: 0;
+  width: 233px;
+  list-style: none;
+}
 
-  li {
-    height: 21px;
-    padding: 5px 8px;
-    border: 1px solid #aaa;
-  }
+li {
+  height: 21px;
+  padding: 5px 8px;
+  border: 1px solid #aaa;
+  background-color: white;
+}
+
+li:hover {
+  background-color: #bbbbbb;
+}
+
+.selected {
+  background-color: #ddddfb;
+}
 </style>
